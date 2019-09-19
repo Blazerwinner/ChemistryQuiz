5e5103b9729f21c1bb3d375a4cc54f55cefc4e4b
Blazerwinner <54326437+Blazerwinner@users.noreply.github.com>
2019-09-19 10:37:34 -0700

    Update JQuery.js

--- JQuery.js
+++ JQuery.js
@@ -1,1 +1,14 @@
+  
+$(document).ready(function(){
 
+  $("button").click(function(){
+    $(".answer").toggle();
+	});
+});
+  
+$(document).ready(function(){
+
+  $("button").click(function(){
+    $(".answer").toggle();
+	});
+});
